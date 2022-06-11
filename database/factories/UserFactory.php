f67c72e559166f39f8dfae66b8fb197fb10377cd
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-06-11 02:02:57 +0100

    implementing factory users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
+use App\Models\User;
 class UserFactory extends Factory
 {
     /**
@@ -20,6 +20,17 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'verified' => $verified = $this->faker->randomElement([
+                User::VERIFIED_USER,
+                User::UNVERIFIED_USER
+            ]),
+
+            "verif_token" => $verified === User::VERIFIED_USER ? null : User::generateVerifCode(),
+            'admin' => $verified = $this->faker->randomElement([
+
+                User::ADMIN_USER,
+                User::REGULAR_USER
+            ])
         ];
     }
 
