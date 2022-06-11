ea518f5ff21bd398afc52c0749e32eb2cff56067
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-06-11 01:19:35 +0100

    configuring models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    const AVAILABLE_PRODUCTS = 'available';
+    const UNAVAILABLE_PRODUCTS = 'unavailable';
+
+    protected $fillable = [
+
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'selled_id',
+        'color'
+    ];
+
+
+    public function isAvailable(){
+
+       return $this->status === Product::AVAILABLE_PRODUCTS;
+    }
+
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        "quantity",
+        'buyer_id',
+        "product_id",
+        'infos'
+    ];
 }
